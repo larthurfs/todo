637826540060bf89ec1cbee240345f312d9789ff
Luiz Arthur <58349638+larthurfs@users.noreply.github.com>
2023-08-24 14:26:14 -0300

    del

--- apps/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from apps.accounts.views import register
-
-urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('register/', register, name='register'),
-
-]
--- apps/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from apps.core.views import home
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('dash/', include('django_plotly_dash.urls')),
-]
